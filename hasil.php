a25da032a1a20fc7692222aeda2eda3e6f79ef8f
wahyuakmal78 <30462465+wahyuakmal78@users.noreply.github.com>
2017-10-23 21:05:11 +0700

    Add files via upload

--- hasil.php
+++ hasil.php
@@ -1,25 +1,53 @@
+<!DOCTYPE html>
 <html>
 <head>
 <title>Untitled Document</title>
 </head>
 
-<body>
-<center>Sign Up berhasil...!<br>
-<table>
-	<?php
-		$nama=$_POST["nama"];
-		$user=$_POST["user"];
-		$email=$_POST["email"];
-		$notelepon=$_POST["notelepon"];
-		$password=$_POST["password"];
-		$password=str_repeat("*",strlen("password"));
+<body>  
+<center>
 
-		echo"<tr><td>Nama</td><td>:</td><td>$nama</td></tr>";
-		echo"<tr><td>Nama Pengguna</td><td>:</td><td>$user</td></tr>";
-		echo"<tr><td>Email</td><td>:</td><td>$email</td></tr>";
-		echo"<tr><td>No Telepon</td><td>:</td><td>$notelepon</td></tr>";
-		echo"<tr><td>Password</td><td>:</td><td>$password</td></tr>";
+	<?php
+
+	$host = "localhost";
+	$user = "root";
+	$password = "";
+
+	$full_name = $_POST['full_name'];
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$no_telp = $_POST['no_telp'];
+	$pass = $_POST['pass'];
+
 	?>
-</table>
+<form method="POST" action="hasil.php">
+	<h1>Sign Up Succes</h1>
+	<table>
+		<tr>
+			<td>Full name</td><td> : </td><td><?php echo "$full_name"; ?></td>
+		</tr>
+		<tr>
+			<td>Username</td><td> : </td><td><?php echo "$username"; ?></td>
+		</tr>
+		<tr>
+			<td>Email</td><td> : </td><td><?php echo "$email"; ?></td>
+		</tr>
+		<tr>
+			<td>No Telepon</td><td> : </td><td><?php echo "$no_telp"; ?></td>
+		<tr>
+			<td>Password</td><td> : </td><td><?php echo "$pass"; ?></td>
+		</tr>	
+		
+		
+	</table>
+	
+</form>
+
+<?php
+	$conn = mysqli_connect($host, $user, $password);
+	mysqli_select_db($conn, 'dbig');
+	$sql = "insert into userig values('$full_name', '$username', '$email', '$no_telp', '$password')";
+	$hasil = mysqli_query($conn,$sql);
+?>
 </body>
 </html>
